b4bf993e6783a73142baa460bdfebc93ae7a147e
Faisal Shaikh <faisalskint@gmail.com>
2026-02-18 12:27:12 +0400

    Added variable "Address"

--- EmployeeManager/Models/Employee.cs
+++ EmployeeManager/Models/Employee.cs
@@ -7,5 +7,6 @@
         public string Email { get; set; }
         public string Department { get; set; }
         public string Phone { get; set; }
+        public string Address { get; set; }
     }
 }
