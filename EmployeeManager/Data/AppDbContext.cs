52ee48764fc987da6ced41a21d12239a2c4c6b2b
Faisal Shaikh <faisalskint@gmail.com>
2026-02-19 10:03:17 +0400

    Added AppDbContext

---
+++ EmployeeManager/Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using EmployeeManager.Api.Models;
+
+namespace EmployeeManager.Api.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
