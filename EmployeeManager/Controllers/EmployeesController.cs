984c9daf14055408327e1499507f904c4c33587c
Faisal Shaikh <faisalskint@gmail.com>
2026-02-21 10:30:18 +0400

    Added EmployeesController

---
+++ EmployeeManager/Controllers/EmployeesController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using EmployeeManager.Api.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmployeeManager.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeesController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public EmployeesController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetEmployees()
+        {
+            var employees = await _context.Employees.ToListAsync();
+            return Ok(employees);
+        }
+    }
+}
