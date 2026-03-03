a1e499930e5c9e6a2748916c35652e731266fe09
Faisal Shaikh <faisalskint@gmail.com>
2026-03-03 10:41:50 +0400

    Added logger

--- EmployeeManager/Middleware/GlobalExceptionMiddleware.cs
+++ EmployeeManager/Middleware/GlobalExceptionMiddleware.cs
@@ -6,10 +6,12 @@ namespace EmployeeManager.Api.Middleware
     public class GlobalExceptionMiddleware
     {
         private readonly RequestDelegate _next;
+        private readonly ILogger<GlobalExceptionMiddleware> _logger;
 
-        public GlobalExceptionMiddleware(RequestDelegate next)
+        public GlobalExceptionMiddleware(RequestDelegate next, ILogger<GlobalExceptionMiddleware> logger)
         {
             _next = next;
+            _logger = logger;
         }
 
         public async Task InvokeAsync(HttpContext context)
@@ -20,6 +22,7 @@ namespace EmployeeManager.Api.Middleware
             }
             catch (Exception ex)
             {
+                _logger.LogError(ex, "Unhandled exception occurred.");
                 context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
                 context.Response.ContentType = "application/json";
 
