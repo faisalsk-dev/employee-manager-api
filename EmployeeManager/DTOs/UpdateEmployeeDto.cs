3a208529d787e73f8d09ab8892011e11d8e0c316
Faisal Shaikh <faisalskint@gmail.com>
2026-03-05 09:12:59 +0400

    Updated validation in UpdateEmployeeDto

--- EmployeeManager/DTOs/UpdateEmployeeDto.cs
+++ EmployeeManager/DTOs/UpdateEmployeeDto.cs
@@ -5,6 +5,7 @@ namespace EmployeeManager.Api.DTOs
     public class UpdateEmployeeDto
     {
         [Required]
+        [StringLength(100, MinimumLength = 2)]
         public string Name { get; set; }
 
         [Required]
@@ -12,12 +13,15 @@ namespace EmployeeManager.Api.DTOs
         public string Email { get; set; }
 
         [Required]
+        [StringLength(50)]
         public string Department { get; set; }
 
         [Required]
+        [Phone]
         public string Phone { get; set; }
 
         [Required]
+        [StringLength(200)]
         public string Address { get; set; }
     }
 }
